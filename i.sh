3bd2f6375ecc5cac5fd1c4db02fb81d15a1d22eb
黃柏澄(風雨) <wartw@users.noreply.github.com>
2019-12-11 22:43:22 +0800

    Update i.sh

--- i.sh
+++ i.sh
@@ -4,6 +4,7 @@ server=$1;
 server1=$2;
 curl -O https://raw.githubusercontent.com/wartw98/sshkey/master/install.sh
 sh install.sh
+
 yum install wget -y && wget https://raw.githubusercontent.com/SuicidalCat/Airport-toolkit/master/ssr_node_c7.sh && chmod +x ssr_node_c7.sh && ./ssr_node_c7.sh<<EOF
 Y
 1
@@ -16,6 +17,11 @@ jxspayuser
 Y
 Y
 Y
-Y
+N
 
 EOF
+sudo pip install requests urllib3 pyOpenSSL --force --upgrade
+sudo pip install --upgrade --force-reinstall 'requests==2.6.0'
+systemctl start ssr_node
+systemctl enable ssr_node
+reboot
