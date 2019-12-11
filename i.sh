168e0c43e4a84dd5250326ac167686740e8c145c
黃柏澄(風雨) <wartw@users.noreply.github.com>
2019-12-11 16:56:01 +0800

    Create i.sh

---
+++ i.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+curl -O https://raw.githubusercontent.com/wartw98/sshkey/master/install.sh
+sh install.sh
+yum install vim wget nload iftop -y
+yum -y groupinstall "Development Tools"
+wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.16.tar.gz 
+tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
+./configure && make -j2 && make install
+echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+ldconfig
+
+cd /root
+yum -y install python-setuptools
+easy_install pipgit clone -b manyuser https://github.com/lizhongnian/shadowsocks.gitcd shadowsocks
+pip install -r requirements.txtcp apiconfig.py userapiconfig.pycp config.json user-config.json
