032af24c8804f762a2cef997df15950bab1915ef
黃柏澄(風雨) <wartw@users.noreply.github.com>
2021-07-06 22:33:30 +0800

    Create mk1.sh

---
+++ mk1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+wget https://download.mikrotik.com/routeros/7.1beta6/chr-7.1beta6.img.zip -O chr.img.zip  && \
+gunzip -c chr.img.zip > chr.img  && \
+mount -o loop,offset=512 chr.img /mnt && \
+ADDRESS=`ip addr show enp1s0 | grep global | cut -d' ' -f 6 | head -n 1` && \
+GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
+echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
+/ip route add gateway=$GATEWAY
+/ip service disable telnet
+/user set 0 name=root password=xxxxxx
+ " > /mnt/rw/autorun.scr && \
+umount /mnt && \
+echo u > /proc/sysrq-trigger && \
+dd if=chr.img bs=1024 of=/dev/vda && \
+echo "sync disk" && \
+echo s > /proc/sysrq-trigger && \
+echo "Sleep 5 seconds" && \
+sleep 5 && \
+echo "Ok, reboot" && \
+echo b > /proc/sysrq-trigger
