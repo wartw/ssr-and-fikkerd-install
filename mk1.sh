357d2165b3a9fb2374e76ca85d36e2d503e19f7b
黃柏澄(風雨) <wartw@users.noreply.github.com>
2021-07-10 07:02:25 +0800

    Update mk1.sh

--- mk1.sh
+++ mk1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://download.mikrotik.com/routeros/6.48.3/chr-6.48.3.img.zip  && \
+wget https://download.mikrotik.com/routeros/6.48.3/chr-6.48.3.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 mount -o loop,offset=512 chr.img /mnt && \
 ADDRESS=`ip addr show ens3 | grep global | cut -d' ' -f 6 | head -n 1` && \
