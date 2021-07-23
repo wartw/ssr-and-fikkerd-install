22837ecc3d7e23ae39ef01624065daf039e8dfae
黃柏澄(風雨) <wartw@users.noreply.github.com>
2021-07-24 04:56:29 +0800

    Update mk1.sh

--- mk1.sh
+++ mk1.sh
@@ -2,7 +2,7 @@
 wget https://download.mikrotik.com/routeros/6.48.3/chr-6.48.3.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 mount -o loop,offset=512 chr.img /mnt && \
-ADDRESS=`ip addr show ens3 | grep global | cut -d' ' -f 6 | head -n 1` && \
+ADDRESS=`ip addr show enp1s0 | grep global | cut -d' ' -f 6 | head -n 1` && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=$GATEWAY
