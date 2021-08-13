d7864d96dcecd95e858913538de9a6eda4bbe52f
黃柏澄(風雨) <wartw@users.noreply.github.com>
2021-08-13 11:56:01 +0800

    Update mk.sh

--- mk.sh
+++ mk.sh
@@ -2,7 +2,7 @@
 wget https://download.mikrotik.com/routeros/6.48.3/chr-6.48.3.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 mount -o loop,offset=512 chr.img /mnt && \
-ADDRESS=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
+ADDRESS=`ip addr show ens3 | grep global | cut -d' ' -f 6 | head -n 1` && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=$GATEWAY
@@ -11,7 +11,7 @@ echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where
  " > /mnt/rw/autorun.scr && \
 umount /mnt && \
 echo u > /proc/sysrq-trigger && \
-dd if=chr.img bs=1024 of=/dev/sda && \
+dd if=chr.img bs=1024 of=/dev/vda && \
 echo "sync disk" && \
 echo s > /proc/sysrq-trigger && \
 echo "Sleep 5 seconds" && \
