797d3c84b1e8e5caf4b0c37d1cbff22f6026758d
黃柏澄(風雨) <wartw@users.noreply.github.com>
2022-09-26 17:16:52 +0800

    Update mk1.sh

--- mk1.sh
+++ mk1.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
-wget https://download.mikrotik.com/routeros/6.48.3/chr-6.48.3.img.zip -O chr.img.zip  && \
+wget https://download.mikrotik.com/routeros/6.49.6/chr-6.49.6.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
-mount -o loop,offset=512 chr.img /mnt && \
-ADDRESS=`ip addr show enp1s0 | grep global | cut -d' ' -f 6 | head -n 1` && \
-GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
-echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
-/ip route add gateway=$GATEWAY
-/ip service disable telnet
-/user set 0 name=root password=xxxxxx
- " > /mnt/rw/autorun.scr && \
-umount /mnt && \
-echo u > /proc/sysrq-trigger && \
-dd if=chr.img bs=1024 of=/dev/vda && \
-echo "sync disk" && \
-echo s > /proc/sysrq-trigger && \
-echo "Sleep 5 seconds" && \
-sleep 5 && \
-echo "Ok, reboot" && \
-echo b > /proc/sysrq-trigger
+dd if=chr.img bs=1024 of=/dev/vda 
+
+一般建立 Vultr VPS 的方式進行，不過在選擇 Server Type 的時候改成選擇 Upload ISO 並選擇 iPXE ， 填入 http://boot.ipxe.org/demo/boot.php 。
